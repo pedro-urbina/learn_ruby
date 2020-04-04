5f50a0766784ff748a0a3120647c49c1da0d0c13
Pedro Urbina <purbina14@gmail.com>
2020-04-04 16:33:36 -0400

    Completed 05_book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,28 @@
 class Book
 # write your code here
+    # def initialize title=""
+    #     @title = title
+    # end
+
+    def title= title=""
+        @title = title
+    end
+
+    def title title=""        
+        little_words = ["and", "over", "the", "in", "of", "a", "an"]        
+        first = true
+    
+        @title_array = @title.split.map! do |word| 
+            if first
+                first = false
+                word.capitalize
+            elsif (not(little_words.any? {|element| element == word}))
+                word.capitalize            
+            else
+                word
+            end
+        end
+    
+        @title_array.join(" ")
+    end
 end
